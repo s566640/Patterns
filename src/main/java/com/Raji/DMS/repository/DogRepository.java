66a0ce5a94a2f9dd8c093d8210201a7ccce2139e
s566640 <s566640@nwmissouri.edu>
2024-04-17 17:16:36 -0500

    files added

---
+++ src/main/java/com/Raji/DMS/repository/DogRepository.java
@@ -0,0 +1,16 @@
+package com.Raji.DMS.repository;
+
+
+
+import org.springframework.data.repository.CrudRepository;
+
+
+
+import com.Raji.DMS.Models.Dog;
+/*
+ * @ author Rajya Lakshmi Ganganaboina
+ */
+
+public interface DogRepository extends CrudRepository<Dog,Integer> {
+	
+}
---
+++ src/main/java/com/Raji/DMS/repository/TrainerRepository.java
@@ -0,0 +1,18 @@
+
+package com.Raji.DMS.repository;
+
+
+
+
+import org.springframework.data.repository.CrudRepository;
+
+
+
+import com.Raji.DMS.Models.Trainer;
+/*
+ * @ author Rajya Lakshmi Ganganaboina
+ */
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer>{
+
+}
